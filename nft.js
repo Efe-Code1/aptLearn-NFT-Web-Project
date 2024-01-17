5bcd0bf6de349b327e13995adf4fb6d0ee92ca9f
Efe-code1 <efezzy321@gmail.com>
2024-01-17 09:56:08 +0100

    halfway done

--- nft.js
+++ nft.js
@@ -1,1 +1,23 @@
-alert("NOTE: This is just a customized mockup-page of an NFT website created by Efe, and so any link clicked on this webpage directs you to the main NFT website");
+// alert("NOTE: This is just a customized mockup-page of an NFT website created by Efe, and so any link clicked on this webpage directs you to the main NFT website");
+
+// const sideBar = () => {
+    function sideBar() {
+    // console.log(event.target);
+    const navlink = document.querySelector(".navlinks");
+    navlink.classList.add("navlinkShow");
+};
+
+    function closeSideBar() {
+    // console.log(event.target);
+    const navlink = document.querySelector(".navlinks");
+    navlink.classList.remove("navlinkShow");
+};
+
+// function sideBar() {
+//     const yes =  document.getElementsByClassName("navlinks");
+//     if (yes.className === "navlinks") {
+//         yes.className += "navlinksShown";
+//     } else{
+//         yes.className = "navlinks;"
+//     }
+// }
